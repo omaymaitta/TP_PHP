3f525382f8d87510e6b6a592919b61e670529f50
omaymaitta <62363101+omaymaitta@users.noreply.github.com>
2021-06-20 12:14:04 +0100

    Add files via upload

---
+++ mabase.sql
@@ -0,0 +1,36 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : Dim 20 juin 2021 à 12:58
+-- Version du serveur :  10.4.11-MariaDB
+-- Version de PHP : 7.4.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `mabase`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+-- Erreur de lecture de structure pour la table mabase.user : #1932 - Table 'mabase.user' doesn't exist in engine
+-- Erreur de lecture des données pour la table mabase.user : #1064 - Erreur de syntaxe près de 'FROM `mabase`.`user`' à la ligne 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
